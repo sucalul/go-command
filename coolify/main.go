aedff876dcd9414b3273a84a51dcbf8ab58fd186
yuya <cheeeese4869@gmail.com>
2021-10-12 18:32:24 +0900

    add

---
+++ coolify/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+const (
+	duplicateVowel bool = true
+	removeVoewl    bool = true
+)
+
+func randBool() bool {
+	return rand.Intn(2) == 0
+}
+
+func duplicateVowel(word []byte, i int) []byte {
+	return append(word[:i+1], word[i:]...)
+}
+
+func removeVowel(word []byte, i int) []byte {
+	if len(word) > 0 {
+		return word
+	} else {
+		return append(word[:i], word[i+1:]...)
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := []byte(s.Text())
+		if randBool() {
+			var vI int = -1
+			for i, char := range word {
+				switch char {
+				case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+					if randBool() {
+						vI = i
+					}
+				}
+
+				if vI >= 0 {
+					if randBool() {
+						word = duplicateVowel(word, vI)
+					} else {
+						word = removeVowel(word, vI)
+					}
+				}
+
+			}
+		}
+		fmt.Println(string(word))
+	}
+}
